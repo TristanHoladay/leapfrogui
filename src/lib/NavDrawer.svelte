3e75e3b6f47eaf66ba5a046ecd4d00edd8f60765
TristanHoladay <40547442+TristanHoladay@users.noreply.github.com>
2023-09-21 08:52:51 -0600

    initial commit for leapfrogui

---
+++ src/lib/NavDrawer.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { Drawer, IconButton } from '@uui';
+
+	type $$Props = { isDrawerOpen: boolean };
+	export let isDrawerOpen = false;
+
+	$: iconContent = isDrawerOpen ? 'close' : 'menu';
+</script>
+
+<Drawer
+	elevation={2}
+	open={isDrawerOpen}
+	ssx={{ $self: { width: '500px', height: 'calc(100vh - 64px)' } }}
+	rail
+>
+	<IconButton
+		iconClass="material-symbols-outlined"
+		{iconContent}
+		slot="header"
+		on:click={() => (isDrawerOpen = !isDrawerOpen)}
+	/>
+</Drawer>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import '../app.css';
+	import 'material-symbols/';
+	import Navbar from '$lib/Navbar.svelte';
+	import { Box, Drawer, IconButton, ListItem, Theme } from '@uui';
+	import NavDrawer from '$lib/NavDrawer.svelte';
+
+	let windowWidth: number;
+
+	$: isDrawerOpen = windowWidth && windowWidth > 900 ? true : false;
+</script>
+
+<svelte:window bind:innerWidth={windowWidth} />
+<Theme theme="dark" preferredTheme={false}>
+	<Navbar>
+		<IconButton
+			on:click={() => (isDrawerOpen = !isDrawerOpen)}
+			iconContent="menu"
+			iconClass="material-symbols-outlined"
+			ssx={{ $md: { $self: { display: 'none !important' } } }}
+		/>
+	</Navbar>
+	<NavDrawer {isDrawerOpen} />
+	<main>
+		<slot />
+	</main>
+</Theme>
